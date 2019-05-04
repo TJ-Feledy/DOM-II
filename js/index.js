6b05ad4b595f3c20d4da4fe4ebaee00f01a7d565
teej387 <teej387@yahoo.com>
2019-05-03 20:17:19 -0500

    added mouseover animation event to logo-heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', () => {
+    logoHeading.animate({
+        opacity: [0,1],
+        backgroundColor: ['white', 'lightgray','darkgray'],
+    },
+    3000
+    );
+});
