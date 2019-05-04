d1d4696e828b4af565d0132263fc2c6452c79fda
teej387 <teej387@yahoo.com>
2019-05-03 21:35:32 -0500

    added wheel eventlistener

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 // Your code goes here
+console.log('Press any Key to refresh page.')
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('mouseover', () => {
     logoHeading.animate({
         opacity: [0,1],
         backgroundColor: ['white', 'lightgray','darkgray'],
+        color: ['darkgray', 'gray', 'white']
     },
-    3000
+    4000
     );
 });
 
 window.addEventListener('keydown', () => {
     document.location.reload();
-})
+});
+
+const html = document.querySelector('html');
+document.addEventListener('wheel', (event) => {
+    console.log(`You just performed an ${event}!`);
+});
