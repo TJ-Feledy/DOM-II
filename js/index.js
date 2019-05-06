21fffc0182a0f5ad337fda145ec6d420ddd80d3a
teej387 <teej387@yahoo.com>
2019-05-06 17:26:05 -0500

    added drag/drop listener to .content-destination h2

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ logoHeading.addEventListener('mouseover', () => {
         backgroundColor: ['white', 'lightgray','darkgray'],
         color: ['darkgray', 'gray', 'white']
     },
-    4000
+    3000
     );
 });
 
@@ -19,3 +19,33 @@ const html = document.querySelector('html');
 document.addEventListener('wheel', (event) => {
     console.log(`You just performed an ${event}!`);
 });
+
+let dragPick = document.querySelector('.content-destination h2');
+document.addEventListener('drag', (event) => {}, false);
+document.addEventListener('dragstart', (event) => {
+    dragPick = event.target;
+    event.target.style.opacity = .5;
+}, false);
+document.addEventListener('dragend', (event) => {
+    event.target.style.opacity = '';
+}, false);
+document.addEventListener('dragover', (event) => {
+    event.preventDefault();
+}, false);
+document.addEventListener('dragenter', (event) => {
+    if (event.target.className == 'drop') {
+        event.target.style.opacity = .5;
+    }
+}, false);
+document.addEventListener('dragleave', (event) => {
+    if (event.target.className == 'drop') {
+        event.target.style.opacity = '';
+    }
+}, false);
+document.addEventListener('drop', (event) => {
+    event.preventDefault();
+    if (event.target.className == 'drop') {
+        event.target.style.opacity = '';
+        console.log('Have Fun!');
+    }
+}, false);
